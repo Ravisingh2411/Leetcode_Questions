a0d75c38e117d8736141d9e96a4232b2d609d765
Ravisingh2411 <singhravivinay2411@gmail.com>
2026-09-02 16:53:03 +0530

    Time: 6 ms (99.63%), Space: 46.6 MB (49.68%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -0,0 +1,54 @@
+class Solution {
+    class Pair implements Comparable<Pair> {
+        String word;
+        int freq;
+        Pair(String word, int freq) {
+            this.word = word;
+            this.freq = freq;
+        }
+        public int compareTo(Pair p){
+            if (this.freq == p.freq) {
+                return p.word.compareTo(this.word);
+            }
+            return this.freq - p.freq;
+        }
+    }
+    public List<String> topKFrequent(String[] words, int k) {
+        List<String> ans = new ArrayList<>();
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (String s : words) {
+            map.put(s, map.getOrDefault(s, 0) + 1);
+        }
+
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+
+        for (String s : map.keySet()) {
+            int freq = map.get(s);
+            if (pq.size() < k) {
+                pq.add(new Pair(s, freq));
+                continue;
+            }
+
+            Pair top = pq.peek();
+
+            if (freq > top.freq) {
+                pq.remove();
+                pq.add(new Pair(s, freq));
+            }
+
+            // Same frequency, current word is lexicographically smaller
+            else if (freq == top.freq && s.compareTo(top.word) < 0) {
+                pq.remove();
+                pq.add(new Pair(s, freq));
+            }
+        }
+
+        while (pq.size() > 0) {
+            Pair top = pq.remove();
+            ans.add(0, top.word);
+        }
+        return ans;
+    }
+}
