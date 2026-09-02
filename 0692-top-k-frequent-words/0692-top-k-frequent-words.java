5acdf0474b2423c55b44f4cb77a06aede23cf7ef
Ravisingh2411 <singhravivinay2411@gmail.com>
2026-09-03 00:28:43 +0530

    Time: 6 ms (99.63%), Space: 46.6 MB (49.68%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.java
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -26,25 +26,12 @@ class Solution {
 
         for (String s : map.keySet()) {
             int freq = map.get(s);
-            if (pq.size() < k) {
+            if(pq.size() < k) pq.add(new Pair(s, freq));
+            else{
                 pq.add(new Pair(s, freq));
-                continue;
-            }
-
-            Pair top = pq.peek();
-
-            if (freq > top.freq) {
                 pq.remove();
-                pq.add(new Pair(s, freq));
-            }
-
-            // Same frequency, current word is lexicographically smaller
-            else if (freq == top.freq && s.compareTo(top.word) < 0) {
-                pq.remove();
-                pq.add(new Pair(s, freq));
             }
         }
-
         while (pq.size() > 0) {
             Pair top = pq.remove();
             ans.add(0, top.word);
