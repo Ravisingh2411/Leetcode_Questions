a3d0c6f1e195e6aee5f8e9761e77601b4d912a19
Ravisingh2411 <singhravivinay2411@gmail.com>
2026-09-03 00:25:27 +0530

    Time: 13 ms (80.06%), Space: 48 MB (13.76%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,34 +1,33 @@
 class Solution {
     class Pair implements Comparable<Pair>{
-    int ele;
-    int freq;
-    Pair(int ele, int freq){
-        this.ele = ele;
-        this.freq = freq;
+        int ele;
+        int freq;
+        Pair(int ele, int freq){
+            this.ele = ele;
+            this.freq = freq;
+        }
+        public int compareTo(Pair p){
+            if(this.freq == p.freq) return this.ele - p.ele;
+            return this.freq - p.freq;
+        }
     }
-    public int compareTo(Pair p){
-        if(this.freq == p.freq) return this.ele - p.ele;
-        return this.freq - p.freq;
-    }
-}
     public int[] topKFrequent(int[] arr, int k){
         HashMap<Integer,Integer> map = new HashMap<>();
         for(int ele : arr){
             map.put(ele,map.getOrDefault(ele,0)+1);
         }
+
         PriorityQueue<Pair> pq = new PriorityQueue<>(); // minheap
+
         for(int ele : map.keySet()){
             int freq = map.get(ele);
-            if(pq.size() < k){
-                pq.add(new Pair(ele,freq));
-                continue;
-            }
-            if(freq < pq.peek().freq) continue;
+            if(pq.size() < k) pq.add(new Pair(ele, freq));
             else{
-                pq.remove();
                 pq.add(new Pair(ele, freq));
+                pq.remove();
             }
         }
+        
         int[] ans = new int[k];
         int index = 0;
         while(pq.size()>0){
