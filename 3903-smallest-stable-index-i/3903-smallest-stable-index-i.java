a2d24e45c7b0a3d3d2aab8721ac6601b3d827013
Ravisingh2411 <singhravivinay2411@gmail.com>
2026-09-05 01:55:53 +0530

    Time: 1 ms (99.79%), Space: 46.3 MB (70.95%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
@@ -1,15 +1,35 @@
 class Solution {
+    // public int firstStableIndex(int[] nums, int k) {
+    //     int n = nums.length;
+    //     int maxi = nums[0];
+
+    //     for (int i = 0; i < n; i++) {
+    //         maxi = Math.max(maxi, nums[i]);
+    //         int min = nums[i];
+    //         for (int j = i; j < n; j++) {
+    //             min = Math.min(min, nums[j]);
+    //         }
+    //         if (maxi - min <= k) {
+    //             return i;
+    //         }
+    //     }
+    //     return -1;
+    // }
+
+
     public int firstStableIndex(int[] nums, int k) {
         int n = nums.length;
-        int maxi = nums[0];
+        int[] suffixMin = new int[n];
 
+        suffixMin[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            suffixMin[i] = Math.min(nums[i], suffixMin[i + 1]);
+        }
+
+        int prefixMax = nums[0];
         for (int i = 0; i < n; i++) {
-            maxi = Math.max(maxi, nums[i]);
-            int min = nums[i];
-            for (int j = i; j < n; j++) {
-                min = Math.min(min, nums[j]);
-            }
-            if (maxi - min <= k) {
+            prefixMax = Math.max(prefixMax, nums[i]);
+            if (prefixMax - suffixMin[i] <= k) {
                 return i;
             }
         }
