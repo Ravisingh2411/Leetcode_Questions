ddf250f6a924dbd323bb3b8e7cdad1375e48c447
Ravisingh2411 <singhravivinay2411@gmail.com>
2026-09-14 21:44:06 +0530

    Time: 3 ms (94.38%), Space: 47 MB (71.31%) - LeetHub

---
+++ 2974-minimum-number-game/2974-minimum-number-game.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] numberGame(int[] nums) {
+        int[] arr = new int[nums.length];
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int x : nums) pq.add(x);
+        int i = 0;
+        while(pq.size() > 0){
+            int alice = pq.poll();
+            int bob = pq.poll();
+            arr[i++] = bob;
+            arr[i++] = alice;
+        }
+        return arr;
+    }
+}
