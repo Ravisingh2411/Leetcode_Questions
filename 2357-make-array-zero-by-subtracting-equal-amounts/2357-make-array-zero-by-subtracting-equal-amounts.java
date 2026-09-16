5a79b8879290fe0210c1d8dc5ac01b068871cf95
Ravisingh2411 <singhravivinay2411@gmail.com>
2026-09-16 15:19:40 +0530

    Time: 1 ms (91.93%), Space: 42.4 MB (98.59%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.java
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.java
@@ -1,30 +1,39 @@
 class Solution {
     public int minimumOperations(int[] nums) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for (int x : nums) {
+        // PriorityQueue<Integer> pq = new PriorityQueue<>();
+        // for (int x : nums) {
+        //     if(x > 0){
+        //         pq.add(x);
+        //     }
+        // }
+        // int ans = 0;
+        // while(!pq.isEmpty()){
+        //     int ele = pq.peek();
+
+        //     while (!pq.isEmpty()) {
+        //         pq.remove();
+        //     }
+
+        //     ans++;
+        //     for(int i = 0; i < nums.length; i++){
+        //         if(nums[i] > 0){
+        //             nums[i] = nums[i] - ele;
+
+        //             if(nums[i] > 0){
+        //                 pq.add(nums[i]);
+        //             }
+        //         }
+        //     }
+        // }
+        // return ans;
+
+
+        HashSet<Integer> set = new HashSet<>();
+        for(int x : nums){
             if(x > 0){
-                pq.add(x);
+                set.add(x);
             }
         }
-        int ans = 0;
-        while(!pq.isEmpty()){
-            int ele = pq.peek();
-
-            while (!pq.isEmpty()) {
-                pq.remove();
-            }
-
-            ans++;
-            for(int i = 0; i < nums.length; i++){
-                if(nums[i] > 0){
-                    nums[i] = nums[i] - ele;
-
-                    if(nums[i] > 0){
-                        pq.add(nums[i]);
-                    }
-                }
-            }
-        }
-        return ans;
+        return set.size();
     }
 }
